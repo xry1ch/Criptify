cea0d86663f03d3c8bb459d0009af8ef578f5604
Wrynch <48221676+Wrynch@users.noreply.github.com>
2023-04-08 15:19:28 +0200

    Initial

---
+++ bin/decx.py
@@ -0,0 +1,161 @@
+from cryptography.fernet import Fernet
+import os
+import tkinter as tk
+from tkinter import filedialog
+import time
+
+logo = "\033[32m" + """
+
+ ▄████████    ▄████████  ▄█     ▄███████▄     ███      ▄█     ▄████████ ▄██   ▄   
+███    ███   ███    ███ ███    ███    ███ ▀█████████▄ ███    ███    ███ ███   ██▄ 
+███    █▀    ███    ███ ███▌   ███    ███    ▀███▀▀██ ███▌   ███    █▀  ███▄▄▄███ 
+███         ▄███▄▄▄▄██▀ ███▌   ███    ███     ███   ▀ ███▌  ▄███▄▄▄     ▀▀▀▀▀▀███ 
+███        ▀▀███▀▀▀▀▀   ███▌ ▀█████████▀      ███     ███▌ ▀▀███▀▀▀     ▄██   ███ 
+███    █▄  ▀███████████ ███    ███            ███     ███    ███        ███   ███ 
+███    ███   ███    ███ ███    ███            ███     ███    ███        ███   ███ 
+████████▀    ███    ███ █▀    ▄████▀         ▄████▀   █▀     ███         ▀█████▀  
+             ███    ███                                                           
+                                     
+""" + "\033[0m"
+# clear console command
+def clear(): return os.system('cls' if os.name == 'nt' else 'clear')
+
+# Function to generate a Fernet key
+def generate_key():
+    key = Fernet.generate_key()
+    return key
+
+# Function to encrypt a file using a Fernet key
+def encrypt_file(key, filename):
+    fernet = Fernet(key)
+
+    with open(filename, 'rb') as f:
+        data = f.read()
+
+    encrypted_data = fernet.encrypt(data)
+    encrypted_filename = filename + '.enc'
+
+    with open(encrypted_filename, 'wb') as f:
+        f.write(encrypted_data)
+
+    return encrypted_filename
+
+# Function to decrypt a file using a Fernet key
+def decrypt_file(key, filename):
+    fernet = Fernet(key)
+
+    with open(filename, 'rb') as f:
+        encrypted_data = f.read()
+
+    try:
+        decrypted_data = fernet.decrypt(encrypted_data)
+    except:
+        return None
+
+    decrypted_filename = os.path.splitext(filename)[0]
+
+    with open(decrypted_filename, 'wb') as f:
+        f.write(decrypted_data)
+
+    return decrypted_filename
+
+
+# Loop until the user chooses to quit
+while True:
+    # clear the console on program launch
+    clear()
+    print(logo)
+    # Ask the user if they want to encrypt or decrypt files
+    mode = input(
+        "⚡ Enter 1 to encrypt files, 2 to decrypt files, or q to quit: ")
+
+    if mode == '1':
+        # Ask the user to select a file to encrypt
+        root = tk.Tk()
+        root.withdraw()
+        filenames = filedialog.askopenfilenames()
+        if not filenames:
+            print("⛔ No files selected.")
+            time.sleep(3)
+            continue
+        
+        # Ask the user if they want to generate a new key or enter an existing one
+        key_choice = input(
+            "⚡ Enter 1 to generate a new 🔑 or 2 to enter an existing 🔑: ")
+        if key_choice == '1':
+            # Generate a new Fernet key
+            key = generate_key()
+            print("🔑 Key:", key.decode())
+            print("· Press Enter to continue...")
+            input()  # Waits for the user to press enter or any key
+            print("")
+        elif key_choice == '2':
+            # Ask the user for a key
+            key = input("🔑 Enter the key: ")
+            if not key:
+                print("⛔ No key entered.")
+                continue
+            try:
+                key = key.encode()
+                fernet = Fernet(key)
+            except ValueError:
+                print("⛔ Invalid key. Please enter a valid Fernet key.")
+                time.sleep(3)
+                continue
+        else:
+            print(
+                "⚠️ Invalid choice. Please enter 1 to generate a new key or 2 to enter an existing key.")
+            time.sleep(3)
+            continue
+
+        for filename in filenames:
+            # Encrypt the file
+            encrypted_file = encrypt_file(key, filename)
+
+            # Print the encrypted file name
+            print(f"🔒 Encrypted: {filename}")
+
+            # Delete the original file
+            os.remove(filename)
+
+            time.sleep(3)
+
+    elif mode == '2':
+        # Ask the user to select files to decrypt
+        root = tk.Tk()
+        root.withdraw()
+        filenames = filedialog.askopenfilenames()
+        if not filenames:
+            print("⛔ No files selected.")
+            time.sleep(3)
+            continue
+
+        # Ask the user for the key
+        key = input("🔑 Enter the key: ")
+        if not key:
+            print("⛔ No key entered.")
+            time.sleep(3)
+            continue
+
+        # Decrypt the files
+        for filename in filenames:
+            decrypted_file = decrypt_file(key.encode(), filename)
+
+            if decrypted_file is None:
+                print(
+                    f"⛔ Failed to decrypt the file {filename}. Please check the decryption key and try again.")
+                time.sleep(3)
+            else:
+                # Print the decrypted file name
+                print("🔓 Decrypted file:", decrypted_file)
+
+                # Delete the original encrypted file
+                os.remove(filename)
+
+                time.sleep(3)
+
+    elif mode == 'q':
+        break
+
+    else:
+        print("⚠️ Invalid mode. Please enter 1 to encrypt files, 2 to decrypt files, or q to quit.")
